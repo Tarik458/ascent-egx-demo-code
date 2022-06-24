316a15266fc22f8457af95884f7518ec01ff95c8
Tarik Sabin <tarik.sabin@gmail.com>
2022-06-24 19:18:26 +0100

    created a smoother follow cam, removed prototype

--- Assets/Ascent/PilgrimScripts/PilgrimFollowCam.cs
+++ Assets/Ascent/PilgrimScripts/PilgrimFollowCam.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PilgrimFollowCam : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    [Tooltip("The target for the camera to follow (the pilgrim)")]
+    private Transform Target;
+
+    [SerializeField]
+    [Tooltip("The speed at which the camera will react to the target's movement, default 1")]
+    private float FollowSpeed = 1f;
+
+    /// <summary>
+    /// Camera position in relation to the target object. Should be updated any time the camera position is changed.
+    /// </summary>
+    private Vector3 camOffset;
+
+    private void Start()
     {
-        
+        // Get initial camera offset.
+        camOffset = transform.position - Target.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Vector3 camDesiredPos = Target.position + camOffset;
+        transform.position = Vector3.Lerp(transform.position, camDesiredPos, FollowSpeed * Time.deltaTime);
     }
 }
