6ce851e2caf0f3094271ce0b555b80f25eb30665
Tarik Sabin <tarik.sabin@gmail.com>
2022-06-24 13:35:55 +0100

    Formatted script, added tmp face mat

--- Assets/Ascent/Input/PilgrimController.cs
+++ Assets/Ascent/Input/PilgrimController.cs
@@ -5,35 +5,46 @@ using UnityEngine.InputSystem;
 
 public class PilgrimController : MonoBehaviour
 {
-
-    public float moveSpeed = 2f;
+    [SerializeField]
+    [Tooltip("Modifier to change the walkspeed of the pilgrim")]
+    private float MoveSpeed = 2.5f;
+    [SerializeField]
+    [Tooltip("The visual component of the pilgrim to be rotated to face the direction of travel")]
+    private GameObject VisualComponent;
 
     private Vector2 moveDirection;
 
-
-    private PlayerControls controls;
+    /// <summary>
+    /// Use Controls not _controls
+    /// </summary>
+    private PlayerControls _controls;
+    /// <summary>
+    /// Use Controls not _controls
+    /// </summary>
     private PlayerControls Controls
     {
         get
         {
-            if (controls != null)
+            if (_controls != null)
             {
-                return controls;
+                return _controls;
             }
-            return controls = new PlayerControls();
+            return _controls = new PlayerControls();
         }
     }
 
 
     private void Awake()
     {
+        // Set up event triggers for movement.
         Controls.Pilgrim.Movement.performed += ctx => OnMovement(ctx.ReadValue<Vector2>());
         Controls.Pilgrim.Movement.canceled += ctx => OnMovement(ctx.ReadValue<Vector2>());
     }
 
     private void Update()
     {
-        transform.Translate(new Vector3(moveDirection.x, 0f, moveDirection.y) * moveSpeed * Time.deltaTime);
+        // Move the pilgrim smoothly in correct direction.
+        transform.Translate(new Vector3(moveDirection.x, 0f, moveDirection.y) * MoveSpeed * Time.deltaTime);
     }
 
 
