0de91212827505ccb032b0952f92186b42e10f99
Tarik Sabin <tarik.sabin@gmail.com>
2022-07-26 12:45:28 +0100

    end screen stuff added

---
+++ Assets/Ascent/Scripts/ShowEndscreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowEndscreen : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas EndScreenCanvas;
+
+
+    public void ShowCanvas()
+    {
+
+    }
+
+    public void QuitToMenu()
+    {
+
+    }
+
+}
