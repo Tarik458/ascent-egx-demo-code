264eae7f6c0ef34c8428984a5f302d0e63a129fe
Tarik Sabin <tarik.sabin@gmail.com>
2022-08-24 17:40:46 +0100

    fade in endscreen

--- Assets/Ascent/Scripts/ShowEndscreen.cs
+++ Assets/Ascent/Scripts/ShowEndscreen.cs
@@ -9,13 +9,38 @@ public class ShowEndscreen : MonoBehaviour
     private GameObject EndScreenCanvas;
 
     [SerializeField]
+    private float FadeInTime = 2.5f;
+
+    [SerializeField]
     AudioSource audioSource;
 
     public void ShowCanvas()
     {
-        EndScreenCanvas.SetActive(true);
+
+        StartCoroutine(FadeInCanvas());
         audioSource.Play();
     }
+
+    private IEnumerator FadeInCanvas()
+    {
+        EndScreenCanvas.SetActive(true);
+        CanvasGroup canvasGroup = EndScreenCanvas.GetComponent<CanvasGroup>();
+        float startVal = 0f;
+        float endval = 1f;
+        float timeElapsed = 0f;
+        canvasGroup.alpha = startVal;
+
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha = Mathf.Lerp(startVal, endval, timeElapsed / FadeInTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        canvasGroup.alpha = endval;
+    }
+
+
 
     public void QuitToMenu()
     {
